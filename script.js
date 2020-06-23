2ae354ada718f594766589e1a2f394cff3b55a13
Orkhan Huseynov <orkhanhuseynov@Orkhans-iMac.local>
2020-06-23 14:54:04 +0400

    fix

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight;
+    console.log(vh);
+    
+    // document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
