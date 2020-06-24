ca6df6bf3099c17a884ffde0ead3793ffa25e30d
Orkhan Huseynov <orkhanhuseynov@Orkhans-iMac.local>
2020-06-24 12:13:34 +0400

    fixing notification js

--- script.js
+++ script.js
@@ -7,30 +7,46 @@ class ScreenSize {
     }
 }
 
-
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
         setMobileViewportHeight();
-        showNotification();
+        showNotification()();
     };
 }
+window.addEventListener('resize', () => {
+    setMobileViewportHeight();
+    showNotification()();  
+})
 
 function setMobileViewportHeight() {
     let el = document.getElementById('container');
     el.style.setProperty('--vh', `${ScreenSize.height()}px`);
 }
 
-window.addEventListener('resize', () => {
-    setMobileViewportHeight();
-    showNotification();
 
-})
-
-function showNotification() {
-    let el = document.getElementById('notification')
-    let v = window.matchMedia("(orientation: portrait)").matches;
-    viewMode = (v) ? 'Portrait' : 'Landscape';
-    el.textContent = `${viewMode} mode: ${ScreenSize.height()}x${ScreenSize.width()}`;
-    el.className = 'show';
-    setTimeout(() => { el.className = 'hide' }, 3000);
+function showNotification(){
+    let timeoutId;
+    return () => {
+        clearTimeout(timeoutId);
+        let el = createElement('div','notification');
+        let v = window.matchMedia("(orientation: portrait)").matches;
+        viewMode = (v) ? 'Portrait' : 'Landscape';
+        el.textContent = `${viewMode} mode: ${ScreenSize.height()}x${ScreenSize.width()}`;
+        el.className = 'show';
+        timeoutId = setTimeout(() => { el.className = 'hide' }, 2000);
+    }
 }
+
+function createElement(elementName, elemendId = ''){
+    if (elemendId != '') {
+        let foundEl = document.getElementById(elemendId);
+        if (foundEl) {
+            return foundEl;
+        }
+    } 
+    let el = document.createElement(elementName);
+    el.setAttribute('id',elemendId);
+    document.body.appendChild(el);
+    return el;
+}
+
