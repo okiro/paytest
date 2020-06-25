5104800825c0bdd1c36f2a88a6ccad2f6dbc6a3d
Orkhan Huseynov <orkhanhuseynov@Orkhans-iMac.local>
2020-06-25 15:22:38 +0400

    cv

--- src/js/cardValidation.js
+++ src/js/cardValidation.js
@@ -3,7 +3,16 @@ export function cardValidation(){
     for (let el of inputFields){
         el.addEventListener('input', e => {
             el.value = el.value.replace(/\D/,'');
-            el.value = el.value.replace(/(\d{4}\B)/g, '$1 ');
+            cardNumber(el);
+            expDate(el);
         })
     }
+}
+
+
+function cardNumber(el){
+    el.value = el.value.replace(/(\d{4}\B)/g, '$1 ');
+}
+function expDate(el){
+
 }
