853672eb0d5ccb3c4fc175c034106325f3e467a1
Orkhan Huseynov <orkhanhuseynov@Orkhans-MacBook-Pro.local>
2020-06-27 00:36:20 +0400

    fxing issues

--- src/js/DomManipulation.js
+++ src/js/DomManipulation.js
@@ -5,10 +5,12 @@ export class DomManipulation {
 
     addErrorClassName(element) {
         element.classList.add('inputErrorIcon');
+        document.getElementById('inputError').style.visibility = 'visible';
     }
 
     removeErrorClassName(element) {
         element.classList.remove('inputErrorIcon');
+        document.getElementById('inputError').style.visibility = 'hidden';
     }
 
 
