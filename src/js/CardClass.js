2d005c6b285677729984fd95a8bec241bc550595
Orkhan Huseynov <orkhanhuseynov@Orkhans-MacBook-Pro.local>
2020-06-27 23:38:53 +0400

    cc exp fix

--- src/js/CardClass.js
+++ src/js/CardClass.js
@@ -10,9 +10,31 @@ export class CardClass {
         return true;
     }
 
-    getFormat(str) {
+    getCardFormat(str) {
         str = str.replace(/(\d{4}\B)/g, '$1 ').replace(/ (?!\d)/, '');
         return str;
+    }
+
+    getExpFormat(str, key) {
+        if (key === '/') {
+            str.length === 1 ? str = '' : null;
+            str[0] === '1' && str.length === 2 ? str = '0' + str[0] + '/' : null;
+            str.length > 3 ? str = str.substring(0, str.length - 1) : null;
+        }
+        if (parseInt(str) > 1 && str.length === 1) str = '0' + str[0] + '/';
+        if (parseInt(str) > 12 && str.length === 2) str = '0' + str[0] + '/' + str[1];
+        if (parseInt(str) > 12 && str.length === 2) str = '0' + str[0] + '/' + str[1];
+        if (/\d{3}/.test(str) && str.length === 3) str = str[0] + str[1] + '/' + str[2];
+        if (key != null && str.length == 2) str += '/';
+        return str;
+    }
+
+    isValidExpDate(str) {
+        let month = str.substring(0, 2);
+        let year = str.substring(3, 5);
+        if (parseInt(month) > 12) return false;
+        if (parseInt(year) < 20) return false;
+        return true
     }
 
     isValidNumber(str) {
--- src/js/cardValidation.js
+++ src/js/cardValidation.js
@@ -7,20 +7,37 @@ let card = new CardClass();
 export function cardValidation() {
     let inputFields = document.getElementsByTagName('input');
     for (let el of inputFields) {
-        el.oninput = () => {
-            el.value = el.value.replace(/\D/g, '');
-            el.value = card.getFormat(el.value);
-            card.isValidProvider(el.value) ? dm.removeErrorClassName(el) : dm.addErrorClassName(el);
+        el.oninput = (e) => {
+            if (el.id === 'ccnumber') {
+                el.value = el.value.replace(/\D/g, '');
+                el.value = card.getCardFormat(el.value);
+                card.isValidProvider(el.value) ? dm.removeErrorClassName(el) : dm.addErrorClassName(el);
 
-            if (el.maxLength === el.value.length) {
-                if (card.isValidNumber(el.value.replace(/ /g, ''))) {
-                    dm.removeErrorClassName(el)
-                    dm.focusNext('ccexp');
-                } else {
-                    dm.addErrorClassName(el)
-                };
+                if (el.maxLength === el.value.length) {
+                    if (card.isValidNumber(el.value.replace(/ /g, ''))) {
+                        dm.removeErrorClassName(el)
+                        dm.focusNext('ccexp');
+                    } else {
+                        dm.addErrorClassName(el);
+                    };
+                }
             }
+            if (el.id === 'ccexp') {
+                el.value = el.value.replace(/[^0-9\/]/g, '');
+                el.value = card.getExpFormat(el.value, e.data);
 
+                if (el.maxLength === el.value.length) {
+                    if (card.isValidExpDate(el.value)) {
+                        dm.removeErrorClassName(el)
+                        dm.focusNext('cccvc');
+                    } else {
+                        dm.addErrorClassName(el);
+                    }
+                }
+            }
+            if (el.id === 'cccvc') {
+                el.value = el.value.replace(/\D/g, '');
+            }
         }
     }
 }
